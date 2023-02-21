354e9a6c3c682fe32f1aebe234638cfdbda8bb19
mwihoti <danielmwihoti@gmail.com>
2023-02-21 06:39:33 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * main - print_alphabet
+ * print alphabet in lowercase
+ * Return : none
+ */
+
+void print_alphabet(void)
+{
+	char dan;
+
+	for (dan = 'a'; dan <= 'z'; dan++)
+		_putchar(dan);
+
+	_putchar('\n');
+}
