5b50446bba16b38f10c74826b70bcf83465d96a4
mwihoti <danielmwihoti@gmail.com>
2023-02-27 02:47:54 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - length of a string
+ * @s: char
+ * Return: length
+ */
+int _strlen(char *s);
+{
+	int leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+	return (leng);
+}
