3c97ccb96ee20666ec3e9ccc270c7d78e2222f29
mwihoti <danielmwihoti@gmail.com>
2023-11-23 07:19:41 -0800

    unsingedInt

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,29 +2,21 @@
 
 /**
  * binary_to_uint - converts binary number to unsigned int
- * b: pointer of string chars
- * return: b is NULL or one or more chars in string b
- * 	   otherwise convert number
+ * @b: pointer to string of binary chars
+ * Return: converted number or 0 if b is NULL or contains invalid characters
  */
-unsigned int binary_to_uint(const char *b){
-	 int intLen;
-	unsigned int unInt,
-	num = 0;
+unsigned int binary_to_uint(const char *b)
+{
+	int unInt;
+        unsigned int convNum = 0;
 
-	if (!b)
-		return (0);
-	for (intLen = 0; b[intLen] != '\0';intLen++)
-	{
-		if (b[intLen] != '\0' && b[intLen] != '1')
-			return (0);
-	}
-	for (intLen = 0; b[ intLen] != '0'; intLen++)
-	{
-		unInt <<= 1;
-		if (b[intLen] == '1')
-			num += 1;
-	}
-	return (unInt);
-	
+        if (!b)
+                return (0);
+        for (unInt = 0; b[unInt]; unInt++)
+        {
+                if (b[unInt] < '0' || b[unInt] > '1')
+                        return (0);
+                convNum = 2 * convNum + (b[unInt] - '0');
+        }
+        return (convNum);
 }
-
