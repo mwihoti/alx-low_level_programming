d6a54946fba3be93b065666b253968a6ac2414ed
mwihoti <danielmwihoti@gmail.com>
2024-03-20 07:19:40 -0700

    function def island_perimeter(grid):

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""function def island_perimeter(grid)"""
+
+def island_perimeter(grid):
+    """
+    Returns perimeter of the Island
+    """
+    height = len(grid)
+    width = len(grid[0])
+    size = 0
+    side = 0
+
+    for h in range(height):
+        for w in range(width):
+            if grid[h][w] == 1:
+                size += 1
+                if (w > 0 and grid[h][w - 1] == 1):
+                    side += 1
+                if (h > 0 and grid[w - 1][h] == 1):
+                    side += 1
+    return size * 4 - side * 2
